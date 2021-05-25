0fc064521f9f4504e9ba93875f4abfd76e4aea30
khushihappiness <74294754+khushihappiness@users.noreply.github.com>
2021-05-25 18:36:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:"png",
+    png_quality:90
+});
+ var camera=document.getElementById("camera");
+ Webcam.attach(camera);
+
+ function take_snapshot()
+ {
+     Webcam.snap(function(data_uri)
+     {
+         document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'"/>';
+     });
+
+ }
+ console.log("The ml5 version is",ml5.version);
+
+
+ classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/8hJnLuQy7/model.json",modelLoaded);
+  
+ function modelLoaded()
+ {
+     console.log("Model is loaded");
+ }
